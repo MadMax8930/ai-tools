8a73a3a61169b5d91d7b6bfac39f814d12ae53dc
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-09-05 23:14:55 +0200

    dashboard navigation done

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,17 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import Sidebar from "@/components/sidebar";
+import { useState, useEffect } from "react";
 
 const MobileSidebar = () => {
+   const [isMounted, setIsMounted] = useState(false);
+
+   useEffect(() => {
+      setIsMounted(true);
+   }, []);
+
+   if (!isMounted) return null;
+
    return (
       <Sheet>
          <SheetTrigger>
